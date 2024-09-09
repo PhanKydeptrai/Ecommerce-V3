4cc3ed450c34545a81f9bcc16ea0cc9a8f43ac35
PhanKydeptrai <kyp194490@gmail.com>
2024-09-09 15:51:01 +0700

    Add project files.

---
+++ CustomerUI/Components/Layout/NavBar.razor.cs
@@ -0,0 +1,27 @@
+﻿using Blazored.SessionStorage;
+using Domain.DTO_Models;
+using Domain.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace CustomerUI.Components.Layout
+{
+    public partial class NavBar
+    {
+        [Inject]
+        public ISessionStorageService session { get; set; }
+
+        //OnAfterRenderAsync được gọi khi component NavBar đã được render xong
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                //Lấy thông tin user từ session
+                var check = await session.GetItemAsync<LoginResponse>("UserInfo");
+                //Thông báo cho Blazor rằng trạng thái của component đã thay đổi và cần được render lại.
+                StateHasChanged();
+            }
+        }
+
+
+    }
+}
